6de93066db869c3faeb149a8e20f25aaeece5714
hilmanystrom <nyhi22bc@student.ju.se>
2023-03-28 14:26:17 +0200

    cinnamon bun

--- cinnamonBun.js
+++ cinnamonBun.js
@@ -1,7 +1,22 @@
 background(255, 255, 255);
 
 fill(200, 157, 124);
-ellipse(200, 100, 90, 30);
-ellipse(200, 97, 80, 24);
-ellipse(200, 94, 68, 20);
-ellipse(200, 93, 60, 16);
+ellipse(200, 100, 80, 30);
+ellipse(200, 98, 74, 24);
+ellipse(200, 95, 64, 20);
+ellipse(200, 92, 54, 16);
+ellipse(200, 90, 44, 12);
+
+fill(255, 255, 255);
+ellipse(190, 100, 4, 4);
+ellipse(210, 88, 4, 4);
+ellipse(180, 89, 4, 4);
+ellipse(168, 96, 4, 4);
+ellipse(220, 95, 4, 4);
+ellipse(230, 100, 4, 4);
+ellipse(216, 104, 4, 4);
+ellipse(200, 108, 4, 4);
+ellipse(192, 86, 4, 4);
+ellipse(174, 105, 4, 4);
+ellipse(200, 92, 4, 4);
+ellipse(178, 96, 4, 4);

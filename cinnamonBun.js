548e51940087b2c4f920bb4e56f1eaa5b1662a27
hilmanystrom <nyhi22bc@student.ju.se>
2023-03-30 16:20:38 +0200

    avalanche

--- cinnamonBun.js
+++ cinnamonBun.js
@@ -1,26 +1,33 @@
 background(255, 255, 255);
 
-// function cinnamonBun(x, y) {
-//   push();
-//   translate(x, y);
-fill(200, 157, 124);
-ellipse(200, 100, 80, 30);
-ellipse(200, 98, 74, 24);
-ellipse(200, 95, 64, 20);
-ellipse(200, 92, 54, 16);
-ellipse(200, 90, 44, 12);
-fill(255, 255, 255);
-ellipse(190, 100, 4, 4);
-ellipse(210, 88, 4, 4);
-ellipse(180, 89, 4, 4);
-ellipse(168, 96, 4, 4);
-ellipse(220, 95, 4, 4);
-ellipse(230, 100, 4, 4);
-ellipse(216, 104, 4, 4);
-ellipse(200, 108, 4, 4);
-ellipse(192, 86, 4, 4);
-ellipse(174, 105, 4, 4);
-ellipse(200, 92, 4, 4);
-ellipse(178, 96, 4, 4);
-//   pop();
-// }
+let bunX = 100();
+let bunY = 100();
+
+function cinnamonBun(bunX, bunY) {
+  translate(bunX, bunY);
+  push();
+  fill(200, 157, 124);
+  ellipse(200, 100, 80, 30);
+  ellipse(200, 98, 74, 24);
+  ellipse(200, 95, 64, 20);
+  ellipse(200, 92, 54, 16);
+  ellipse(200, 90, 44, 12);
+  fill(255, 255, 255);
+  ellipse(190, 100, 4, 4);
+  ellipse(210, 88, 4, 4);
+  ellipse(180, 89, 4, 4);
+  ellipse(168, 96, 4, 4);
+  ellipse(220, 95, 4, 4);
+  ellipse(230, 100, 4, 4);
+  ellipse(216, 104, 4, 4);
+  ellipse(200, 108, 4, 4);
+  ellipse(192, 86, 4, 4);
+  ellipse(174, 105, 4, 4);
+  ellipse(200, 92, 4, 4);
+  ellipse(178, 96, 4, 4);
+  pop();
+}
+
+function draw() {
+  cinnamonBun();
+}

061f1e536c12645d9907bfef47e504e14ed5cbbd
hilmanystrom <nyhi22bc@student.ju.se>
2023-03-30 15:59:05 +0200

    hotshot

--- cinnamonBun.js
+++ cinnamonBun.js
@@ -1,12 +1,14 @@
 background(255, 255, 255);
 
+// function cinnamonBun(x, y) {
+//   push();
+//   translate(x, y);
 fill(200, 157, 124);
 ellipse(200, 100, 80, 30);
 ellipse(200, 98, 74, 24);
 ellipse(200, 95, 64, 20);
 ellipse(200, 92, 54, 16);
 ellipse(200, 90, 44, 12);
-
 fill(255, 255, 255);
 ellipse(190, 100, 4, 4);
 ellipse(210, 88, 4, 4);
@@ -20,3 +22,5 @@ ellipse(192, 86, 4, 4);
 ellipse(174, 105, 4, 4);
 ellipse(200, 92, 4, 4);
 ellipse(178, 96, 4, 4);
+//   pop();
+// }
